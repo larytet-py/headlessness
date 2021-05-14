7358ff1d0d3f34872d43e7a3d3c6f843aaa73797
arkady <arkady>
2021-05-14 09:20:58 +0300

    asyncio

--- src/service.py
+++ src/service.py
@@ -1,12 +1,13 @@
+import asyncio
 from urllib.parse import urlparse, parse_qs
 import asyncio
 import logging
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from threading import Semaphore, Thread
 from time import time, sleep
 from os import environ
 from dataclasses import dataclass
 import json
+from aiohttp import web
 
 from process_url import (
     Page,
@@ -58,39 +59,12 @@ def _get_url_parameter(parameters, name, default=""):
         return float(res)
 
 
-class HeadlessnessServer(BaseHTTPRequestHandler):
+class HeadlessnessServer:
     def __init__(self, logger):
         self.logger = logger
         self.ad_block = AdBlock(["./ads-servers.txt", "./ads-servers.he.txt"])
-
-    def __call__(self, *args, **kwargs):
-        """
-        See https://stackoverflow.com/questions/21631799/how-can-i-pass-parameters-to-a-requesthandler
-        """
-        super().__init__(*args, **kwargs)
-
-    # default process at most 1 query
-    _throttle_max, _throttle = 1, Semaphore(1)
-
-    def _400(self, msg):
-        try:
-            self.send_response(400)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(msg.encode("utf-8"))
-            _statistics.resp_400 += 1
-        except Exception as e:
-            self._logger.error(f"Faied to write to remote {e}")
-
-    def _200(self, msg):
-        try:
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(msg.encode("utf-8"))
-            _statistics.resp_200 += 1
-        except Exception as e:
-            self._logger.error(f"Faied to write to remote {e}")
+        # default process at most 1 query
+        self._throttle_max, self._throttle = 1, Semaphore(1)
 
     def _check_throttle(self):
         time_start = time()
@@ -116,14 +90,12 @@ class HeadlessnessServer(BaseHTTPRequestHandler):
         """
         Called after if os.fork() == 0
         """
-        self.logger.info(f"Fetching {self.url}")
+        self.logger.info(f"Fetching {url}")
         page = Page(
             self._logger, timeout=self.timeout, keep_alive=False, ad_block=self.ad_block
         )
-        asyncio_loop = asyncio.get_event_loop()
-        asyncio_loop.run_until_complete(page.load_page(self._transaction_id, self.url))
-
-        report = generate_report(self.url, self._transaction_id, page)
+        await page.load_page(transaction_id, url)
+        report = generate_report(url, transaction_id, page)
         report_str = json.dumps(report, indent=2)
         results["report"] = report_str
 
@@ -132,45 +104,41 @@ class HeadlessnessServer(BaseHTTPRequestHandler):
             _statistics.unknow_post += 1
             err_msg = f"Unknown post {parsed_url.path}"
             self._logger.error(err_msg)
-            self._400(err_msg)
-            return
+            return web.HTTPNotFound(reason=err_msg)
 
         parameters = parse_qs(parsed_url.query)
-        self.url = _get_url_parameter(parameters, "url", None)
-        if self.url is None:
+        url = _get_url_parameter(parameters, "url", None)
+        if url is None:
             _statistics.bad_url_parameters += 1
             err_msg = f"Missing URL parameter {parsed_url.path}"
             self._logger.error(err_msg)
-            self._400(err_msg)
-            return
+            return web.HTTPNotFound(reason=err_msg)
 
         self.timeout = _get_url_parameter(parameters, "timeout", 30.0)
 
         # asyncio releise on signal, requires main thread
         # I use os.fork + UNIX pipe magic
         error, results = AsyncCall(self.logger)(self.timeout, self._fetch_page, {})
-        report = results.get("report", f"Failed for {self.url}, results={results}")
+        report = results.get("report", f"Failed for {url}, results={results}")
         if error is not None:
-            err_msg = f"Fetch failed for {self.url}: {error}"
+            err_msg = f"Fetch failed for {url}: {error}"
             self._logger.error(err_msg)
-            self._400(err_msg)
-            return
+            return web.HTTPNotFound(reason=err_msg)
 
         error = results.get("error", None)
         if error is not None:
-            err_msg = f"Fetch failed for {self.url}: {error}"
+            err_msg = f"Fetch failed for {url}: {error}"
             self._logger.error(err_msg)
-            self._400(err_msg)
-            return
+            return web.HTTPNotFound(reason=err_msg)
 
-        self._200(report)
+        return web.HTTPSuccessful(reason=report)
 
-    def do_POST(self):
-        parsed_url = urlparse(self.path)
+    async def do_POST(self, request):
+        parsed_url = urlparse(request.path_qs)
         parameters = parse_qs(parsed_url.query)
 
-        self._transaction_id = _get_url_parameter(parameters, "transaction_id")
-        self._logger = LoggerAdapter(self.logger, self._transaction_id)
+        transaction_id = _get_url_parameter(parameters, "transaction_id")
+        self._logger = LoggerAdapter(self.logger,transaction_id)
         self._logger.debug(
             "POST request, path %s, headers %s", str(self.path), str(self.headers)
         )
@@ -178,17 +146,11 @@ class HeadlessnessServer(BaseHTTPRequestHandler):
         if not self._check_throttle():
             err_msg = "Too many requests"
             self._logger.error(err_msg)
-            self._400(err_msg)
-            return
+            raise web.HTTPNotFound(reason=err_msg)
 
-        self._process_post(parsed_url)
+        response = self._process_post(parsed_url)
         HeadlessnessServer._throttle.release()
-
-
-def shutdown():
-    global is_running
-    is_running = False
-
+        raise response
 
 def main():
     logger = logging.getLogger("headlessness")
@@ -200,20 +162,17 @@ def main():
 
     http_port = int(environ.get("PORT", 8081))
     http_interface = environ.get("INTERFACE", "0.0.0.0")
-    http_server = ThreadingHTTPServer(
-        (http_interface, http_port), HeadlessnessServer(logger)
-    )
-    http_server_thread = Thread(target=http_server.serve_forever)
-    http_server_thread.start()
     logger.info(f"I am listening {http_interface}:{http_port}")
-    return http_server_thread, http_server
 
+
+    headlessness_server = HeadlessnessServer()
+    app = web.Application()
+    app.router.add_post('/fetch', headlessness_server.do_POST)
+    web.run_app(app, host=http_interface, port=http_port)
 
 if __name__ == "__main__":
     is_running = True
-    http_server_thread, http_server = main()
+    main()
     while is_running:
         sleep(1.0)
         _statistics.timer_1s += 1
-    http_server.shutdown()
-    http_server_thread.join()
